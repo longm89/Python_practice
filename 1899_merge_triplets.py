3f387752bb0dab321568b136e0e30f6dbcd3c1ba
longm89 <longm89@gmail.com>
2021-06-19 19:01:46 +0200

    fixed lines

--- 1899_merge_triplets.py
+++ 1899_merge_triplets.py
@@ -1,11 +1,20 @@
 """
-A triplet is an array of three integers. You are given a 2D integer array triplets, where triplets[i] = [ai, bi, ci] describes the ith triplet. You are also given an integer array target = [x, y, z] that describes the triplet you want to obtain.
+A triplet is an array of three integers. 
+You are given a 2D integer array triplets, 
+where triplets[i] = [ai, bi, ci] describes the ith triplet. 
+You are also given an integer array target = [x, y, z] 
+that describes the triplet you want to obtain.
 
-To obtain target, you may apply the following operation on triplets any number of times (possibly zero):
+To obtain target, you may apply the following operation 
+on triplets any number of times (possibly zero):
 
-Choose two indices (0-indexed) i and j (i != j) and update triplets[j] to become [max(ai, aj), max(bi, bj), max(ci, cj)].
-For example, if triplets[i] = [2, 5, 3] and triplets[j] = [1, 7, 5], triplets[j] will be updated to [max(2, 1), max(5, 7), max(3, 5)] = [2, 7, 5].
-Return true if it is possible to obtain the target triplet [x, y, z] as an element of triplets, or false otherwise.
+Choose two indices (0-indexed) i and j (i != j) 
+and update triplets[j] to become [max(ai, aj), max(bi, bj), max(ci, cj)].
+For example, if triplets[i] = [2, 5, 3] 
+and triplets[j] = [1, 7, 5], triplets[j] will 
+be updated to [max(2, 1), max(5, 7), max(3, 5)] = [2, 7, 5].
+Return true if it is possible to obtain the target triplet [x, y, z] 
+as an element of triplets, or false otherwise.
 
  
 
